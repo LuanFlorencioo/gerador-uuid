f8dd570f4894015a41e63b9b5dae7a91c93fe169
LuanFlorencio <luanflorencio.dev@gmail.com>
2025-04-15 20:40:50 -0300

    feat(components): add Logo component

---
+++ src/components/Logo.vue
@@ -0,0 +1,7 @@
+<script setup>
+import { Fingerprint } from "lucide-vue-next";
+</script>
+
+<template>
+  <Fingerprint :size="48" class="text-accent" />
+</template>
--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-import Title from "./title/Title.vue";
-
-export { Title };
