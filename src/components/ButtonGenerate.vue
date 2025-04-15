47a763aaa96f1001fa6a97a3c265002d7b1807b4
LuanFlorencio <luanflorencio.dev@gmail.com>
2025-04-15 20:43:14 -0300

    feat(components): add ButtonGenerate component

---
+++ src/components/ButtonGenerate.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useUUIDListStore } from "@/stores/uuidList";
+
+const store = useUUIDListStore();
+</script>
+
+<template>
+  <button
+    type="button"
+    @click="store.generateUUID()"
+    class="w-max px-8 py-2 font-bold text-2xl bg-gradient-to-b from-secondary to-secondary/20 hover:from-accent rounded-md flex items-center gap-2 transition duration-500"
+  >
+    Gerar UUIDs
+  </button>
+</template>
