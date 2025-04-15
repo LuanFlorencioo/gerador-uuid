c94a972c5c4a5fd9a3e60d654d7047c79fd8c7fc
LuanFlorencio <luanflorencio.dev@gmail.com>
2025-04-15 20:44:04 -0300

    feat(components): add CardPainel component

---
+++ src/components/CardPainel.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Logo from "@/components/Logo.vue";
+import ButtonGenerate from "@/components/ButtonGenerate.vue";
+</script>
+
+<template>
+  <div class="w-full sm:max-w-[350px] px-4 py-8 sm:px-8 sm:py-24 bg-gradient-to-b from-secondary/10 to-secondary/40 rounded-b-4xl sm:rounded-4xl flex flex-col items-center gap-8 sm:gap-16 selection:bg-accent">
+    <div class="flex flex-col items-center gap-2">
+      <Logo />
+
+      <h1 class="font-bold text-xl">UUID V4</h1>
+    </div>
+
+    <p class="leading-7 text-center">
+      Clique no botão abaixo para gerar UUIDs. Para copiar, basta apenas clicar no UUID desejado quando for gerado.
+    </p>
+
+    <ButtonGenerate />
+  </div>
+</template>
